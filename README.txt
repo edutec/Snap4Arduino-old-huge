bafda0c12fe2a0b4e5e1700158b8c1b905664108
Bernat Romagosa <bromagosa@dosta.net>
2013-10-25 13:02:06 +0200

    First working prototype

---
+++ README.txt
@@ -0,0 +1,10 @@
+Pharo 2.0
+
+This distribution was built October 03, 2013.
+
+Please Note:
+------------
+
+Pharo does not yet natively run on a 64bits Linux. Just install the ia32/i386 runtime libraries and you should be fine. On Ubuntu and other 
+debian-based systems, these libraries are named 'ia32-libs'.
+
---
+++ shared/snap/ws.js
@@ -0,0 +1,39 @@
+var socket = new WebSocket("ws://localhost:4001/s4a");
+
+var analogReadings = [];
+var digitalReadings = [];
+var analogReadingThreadId;
+var digitalReadingThreadId;
+
+var webSocketRefreshInterval = 20; //milliseconds
+
+function requestAnalogReadings() {
+	socket.send('analogReadings');
+}
+
+function requestDigitalReadings() {
+	socket.send('digitalReadings');
+}
+
+function inform(title, message) {
+	(new DialogBoxMorph()).inform(title, message, world);
+}
+
+socket.onmessage = function(message) {
+	messageArray = message.data.split("&");
+	switch (messageArray[0]) {
+		case 'analogReadings':
+			analogReadings = JSON.parse(messageArray[1]);
+			break;
+		case 'digitalReadings':
+			digitalReadings = JSON.parse(messageArray[1]);
+			break;
+	}
+};
+
+socket.onclose = function() { 
+	inform("Connection lost!", "Lost connection to the server.\nPlease make sure the server is running, then reload this page and try again.");
+	if (analogReadingThreadId != null) { clearInterval(analogReadingThreadId) };
+	if (digitalReadingThreadId != null) { clearInterval(digitalReadingThreadId) };
+}
+
