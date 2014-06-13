bdf1f6bb556935055dee5aea94564574e412f742
Bernat Romagosa <bromagosa@dosta.net>
2014-06-13 11:24:11 +0200

    node-webkit is the way

--- makeRelease.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./updateSnap.sh
-./makeWin32Release.sh
-./makeGnuLinuxRelease.sh
-./makeMacRelease.sh
-cp release/win32/snap4arduino.zip ~/Scratch/s4a/web/snap/downloads/win32/snap4arduino.zip
-cp release/gnu/snap4arduino.tar.gz ~/Scratch/s4a/web/snap/downloads/gnu/snap4arduino.tar.gz
-cp release/mac/snap4arduino.zip ~/Scratch/s4a/web/snap/downloads/mac/snap4arduino.zip
