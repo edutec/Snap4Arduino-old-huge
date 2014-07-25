c9c3113a688ec8be57b636e87a2d56fd0c2793f2
Ernesto Laval <ernesto.laval@gmail.com>
2014-07-25 13:44:40 +0100

    JSHint corrections

--- snap/makersThreads.js
+++ snap/makersThreads.js
@@ -1,38 +1,39 @@
+'use strict';
 /**
  * Attempts to connect to the first USB Device available.
  * Assumes that the device has firmata firmware installed
  */
 Process.prototype.makersAutoconnectArduino = function () {	
 	MakerApp.makersStartArduinoAutoConnect();
-}
+};
 
 /**
  * Disconnects current device (attached to board).
  */
 Process.prototype.makersDisConnectArduino = function () {
 	MakerApp.makersStopArduinoAutoConnect();
-}
+};
 
 /**
  * Checks is there is a connected device (if there is a board object).
  */
 Process.prototype.makersIsConnectedArduino = function () {
-	return (board != undefined);
-}
+	return (board !== undefined);
+};
 
 /**
  * Turn on led on pin 13
  */
 Process.prototype.makersLedOn = function () {
 	this.digitalWrite(13,true);
-}
+};
 
 /**
  * Turn off led on pin 13
  */
 Process.prototype.makersLedOff = function () {
 	this.digitalWrite(13,false);
-}
+};
 
 /**
  * Gets potentiometer value (in scale of 1 to 100)
@@ -42,7 +43,7 @@ Process.prototype.makersPotentiometer = function () {
 
 	val = this.reportAnalogReading(1);
 	return Math.round(10*100*val/1023.0)/10;
-}
+};
 
 /**
  * Gets temperature value (in Celcius degrees)
@@ -52,7 +53,7 @@ Process.prototype.makersTemperature = function () {
 
 	val = this.reportAnalogReading(3);
 	return Math.round(10*500*val/1023.0)/10;
-}
+};
 
 /**
  * Gets light value (in scale of 1 to 100)
@@ -62,7 +63,7 @@ Process.prototype.makersLight = function () {
 
 	val = this.reportAnalogReading(2);
 	return Math.round(10*100*val/1023.0)/10;
-}
+};
 
 /**
  * Gets potentiometer value (in scale of 1 to 100)
@@ -72,15 +73,15 @@ Process.prototype.makersAudio = function () {
 
 	val = this.reportAnalogReading(0);
 	return Math.round(10*100*val/1023.0)/10;
-}
+};
 
 /**
  * Sets signal level for the buzzer
  */
 Process.prototype.makersBuzzer = function(buzzlevel) {
-	this.setPinMode(6,['PWM'])
+	this.setPinMode(6,['PWM']);
 	this.pwmWrite(6,buzzlevel);
-}
+};
 
 /**
  * Gets switch on/off state
@@ -92,7 +93,7 @@ Process.prototype.makersSwitch = function() {
 
 	// Invert true/false value
 	return !val;
-}
+};
 
 
 
