231d2a0da9a9d5ea33b1135e8bf9f8fc78e046db
Bernat Romagosa <bromagosa@dosta.net>
2014-07-28 12:17:00 +0200

    MacOSX version working!

--- build-osx.sh
+++ build-osx.sh
@@ -5,6 +5,7 @@ cp -R ../modules/osx/* .
 zip -r app.nw *
 mv app.nw ../release/osx/Snap4Arduino.app/Contents/Resources
 cd ../release/osx
-rm -f Snap4Arduino.dmg
-hdiutil create -volname Snap4Arduino -srcfolder . -ov -format UDZO Snap4Arduino.dmg
+#rm -f Snap4Arduino.dmg
+#hdiutil create -volname Snap4Arduino -srcfolder . -ov -format UDZO Snap4Arduino.dmg
+zip -r Snap4Arduino Snap4Arduino.app
 cd ../..
