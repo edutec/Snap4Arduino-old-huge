8111072c74d11dfdb5b92de74c712a8448d10581
Ernesto Laval <ernesto.laval@gmail.com>
2014-08-07 10:18:49 +0100

    Remove unused (app.nw2) folder from OSX package

--- release/osx/Snap4Arduino.app/Contents/Resources/app.nw2.nw/erase.js
+++
@@ -1,8 +0,0 @@
-var serialPort = require("serialport");
-serialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
-  });
-});
