fe12c7575d262edccba5378de79209a1f3dd20df
Bernat Romagosa <bromagosa@dosta.net>
2014-10-06 12:30:49 +0200

    fix #2, update #3 with new translations

--- snap/s4aObjects.js
+++ snap/s4aObjects.js
@@ -178,43 +178,18 @@ SpriteMorph.prototype.arduinoDisconnect = function() {
 	}
 }
 
-// blockTemplates() proxy
+// Definition of a new Arduino Category
+
+SpriteMorph.prototype.categories.push('arduino');
+SpriteMorph.prototype.blockColor['arduino'] = new Color(64, 136, 182);
+
+// blockTemplates proxy
 
 StageMorph.prototype.originalBlockTemplates = StageMorph.prototype.blockTemplates;
 SpriteMorph.prototype.originalBlockTemplates = SpriteMorph.prototype.blockTemplates;
 
-// Definition of a new Arduino Category
-
-SpriteMorph.prototype.categories =
-    [
-        'motion',
-        'control',
-        'looks',
-        'sensing',
-        'sound',
-        'operators',
-        'pen',
-        'variables',
-        'lists',
-		'arduino',
-        'other'
-    ];
-
-SpriteMorph.prototype.blockColor = {
-    motion : new Color(74, 108, 212),
-    looks : new Color(143, 86, 227),
-    sound : new Color(207, 74, 217),
-    pen : new Color(0, 161, 120),
-    control : new Color(230, 168, 34),
-    sensing : new Color(4, 148, 220),
-    operators : new Color(98, 194, 19),
-    variables : new Color(243, 118, 29),
-    lists : new Color(217, 77, 17),
-	arduino: new Color(64, 136, 182),
-    other: new Color(150, 150, 150)
-};
-
 // Definition of our new primitive blocks
+
 function overridenBlockTemplates(category) {
 	var myself = this;
 
@@ -297,8 +272,6 @@ function overridenBlockTemplates(category) {
 		spec: 'set PWM pin %pwmPin to %n'
 	};
 
-
-
 	// *this* will either be StageMorph or SpriteMorph
 	var blocks = this.originalBlockTemplates(category); 
 
@@ -311,7 +284,6 @@ function overridenBlockTemplates(category) {
 	if (category === 'arduino') {
 		blocks.push(arduinoConnectButton);
         blocks.push(arduinoDisconnectButton);
-        //blocks.push(blockBySelector('connectArduino'));
 		blocks.push('-');
         blocks.push(blockBySelector('setPinMode'));
 		blocks.push('-');
