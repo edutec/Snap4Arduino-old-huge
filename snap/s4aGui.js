cacb27d6120b0f253041ebb881b933a2b3eb91d7
Ernesto Laval <ernesto.laval@gmail.com>
2014-10-09 16:32:41 +0100

    Loads language files in right order (fixes #31)
    Avoids potential problems overriding arduino blocks translations due to
    incorrect order when processing lang-XX.js & s4a-lang-XX.js

--- snap/s4aGui.js
+++ snap/s4aGui.js
@@ -235,44 +235,36 @@ function homePath() {
 /**
  * Override setLanguage function
  */
-IDE_Morph.prototype.setLanguage = function (lang, callback) {
-    var translation = document.getElementById('language'),
-        src = 'lang-' + lang + '.js',
-        myself = this;
-    SnapTranslator.unload();
-    if (translation) {
-        document.head.removeChild(translation);
-    }
-    if (lang === 'en') {
-        return this.reflectLanguage('en', callback);
-    }
-    translation = document.createElement('script');
-    translation.id = 'language';
-    translation.onload = function () {
-        myself.reflectLanguage(lang, callback);
-    };
-    document.head.appendChild(translation);
-    translation.src = src;
 
-    // Load language script for s4a related functions
-    var s4a_translation = document.getElementById('s4a-language'),
-        s4a_src = 's4a-lang-' + lang + '.js',
-        myself = this;
-    SnapTranslator.unload();
-    if (s4a_translation) {
-        document.head.removeChild(s4a_translation);
-    }
-    if (lang === 'en') {
-        return this.reflectLanguage('en', callback);
-    }
-    s4a_translation = document.createElement('script');
-    s4a_translation.id = 's4a-language';
-    s4a_translation.onload = function () {
-        myself.reflectLanguage(lang, callback);
-    };
-    document.head.appendChild(s4a_translation);
-    s4a_translation.src = s4a_src;
+IDE_Morph.prototype.originalSetLanguage = IDE_Morph.prototype.setLanguage;
 
+IDE_Morph.prototype.setLanguage = function(lang, callback) {
+    var myself = this;
+
+    myself.originalSetLanguage(lang, function() {
+        myself.setLanguageS4A(lang, callback);
+    });
+};
+
+IDE_Morph.prototype.setLanguageS4A = function (lang, callback) {
+     // Load language script for s4a related functions
+     var s4a_translation = document.getElementById('s4a-language'),
+         s4a_src = 's4a-lang-' + lang + '.js',
+         myself = this;
+    //SnapTranslator.unload();
+     if (s4a_translation) {
+         document.head.removeChild(s4a_translation);
+     }
+     if (lang === 'en') {
+         return this.reflectLanguage('en', callback);
+     }
+     s4a_translation = document.createElement('script');
+     s4a_translation.id = 's4a-language';
+     s4a_translation.onload = function () {
+         myself.reflectLanguage(lang, callback);
+     };
+     document.head.appendChild(s4a_translation);
+     s4a_translation.src = s4a_src; 
 };
 
 // Fix probelm with connected board when creating a new project 
