aa0967bb17d98695eee2eb4e63f72058d9dbb490
Bernat Romagosa <bromagosa@dosta.net>
2014-10-16 14:05:36 +0200

    build script for win32 installer works straight from GNU/Linux

--- build-win32.sh
+++ build-win32.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+#
+# We make use of the Inno Setup Compiler
+# This script will not work for you if you haven't installed Inno Setup via wine
+#
+# Instructions taken from:
+# https://katastrophos.net/andre/blog/2009/03/16/setting-up-the-inno-setup-compiler-on-debian/
+
+unset DISPLAY  
 cd snap
 rm -Rf node_modules
 cp -R ../modules/win32/* .
 zip -r app.nw *
 mv app.nw ../release/win32
-cd ..
+cd ../release/win32
+rm Snap4Arduino-*.exe
+wine "C:\Program Files\Inno Setup 5\ISCC.exe" "Snap4Arduino.iss"
+zip Snap4Arduino.zip Snap4Arduino-*.exe
+cd ../..
