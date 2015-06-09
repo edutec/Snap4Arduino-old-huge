92c7369fdb7fbddd6691f9a7a834bc0a3c88e678
Bernat Romagosa <bromagosa@dosta.net>
2015-06-09 15:30:34 +0200

    built

--- build-win32.sh
+++ build-win32.sh
@@ -15,6 +15,6 @@ mv app.nw ../release/win32
 cd ../release/win32
 rm Snap4Arduino-*.exe
 rm Snap4Arduino.zip
-wine "C:\Program Files\Inno Setup 5\ISCC.exe" "Snap4Arduino.iss"
+wine "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" "Snap4Arduino.iss"
 zip Snap4Arduino.zip Snap4Arduino-*.exe
 cd ../..
