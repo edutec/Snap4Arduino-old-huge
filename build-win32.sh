7e8257bee653ff6065b79e9c6a31c7e1602ec1eb
kotl <kotl@2x2.cc>
2015-12-21 18:58:00 -0500

    Add support to compile win32 version under Windows

--- build-win32.sh
+++ build-win32.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 #
 # We make use of the Inno Setup Compiler
-# This script will not work for you if you haven't installed Inno Setup via wine
 #
-# Instructions taken from:
+# So, if you are using Linux to cross compile:
+# Please install Inno Setup via wine, instructions taken from:
 # https://katastrophos.net/andre/blog/2009/03/16/setting-up-the-inno-setup-compiler-on-debian/
+#
+# If you are using Windows:
+# Install 32-bit cygwin: https://cygwin.com/install.html (even for 64-bit machine)
+# Install Inno Setup 5 from here: http://www.jrsoftware.org/isdl.php
 
 unset DISPLAY  
 cd snap
@@ -15,6 +19,11 @@ mv app.nw ../release/win32
 cd ../release/win32
 rm Snap4Arduino-*.exe
 rm Snap4Arduino.zip
-wine "C:\Program Files\Inno Setup 5\ISCC.exe" "Snap4Arduino.iss"
+if [ -z "$PROGRAMFILES" ]; then
+  wine "C:\Program Files\Inno Setup 5\ISCC.exe" "Snap4Arduino.iss"
+else
+  sh -c "exec '$PROGRAMFILES\\Inno Setup 5\\ISCC.exe' Snap4Arduino.iss"
+fi
+
 zip Snap4Arduino.zip Snap4Arduino-*.exe
 cd ../..
